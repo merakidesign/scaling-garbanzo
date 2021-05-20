6f65b607857b07e4b8d87bb27cf6f1095567a177
Septimiu Bloj <septimiubloj@gmail.com>
2021-05-20 19:26:43 +0300

    Capitalize home module import

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
-import styles from "../styles/home.module.css"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
