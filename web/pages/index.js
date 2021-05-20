af53fa91abb715d6b38acb01a19a9d82c5bf9a69
Septimiu Bloj <septimiubloj@gmail.com>
2021-05-20 18:31:24 +0300

    Clear starter pages

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import styles from '../styles/home.module.css'
+import Head from "next/head"
+import Layout, { siteTitle } from "../components/layout"
+import styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
@@ -11,9 +11,10 @@ export default function Home() {
       <section className={styles.hero}>
         <p className={styles.subtitle}>An experience design studio</p>
         <p className={styles.title}>
-          Holistic design & intuitive websites for devoted lifestyle and wellness brands
+          Holistic design & intuitive websites for devoted lifestyle and
+          wellness brands
         </p>
       </section>
     </Layout>
-  )
+  );
 }
